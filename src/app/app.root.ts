65b25a56d34e958c43277fc202519cc3e9e92c6f
hspacesi34 <jarry.cedric34@gmail.com>
2025-09-26 15:22:10 +0200

    feat: verifier token selon la route

--- src/app/app.root.ts
+++ src/app/app.root.ts
@@ -1,7 +1,8 @@
 import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterOutlet, Router } from '@angular/router';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { UserService } from './services/user.service';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -12,27 +13,36 @@ import { UserService } from './services/user.service';
 })
 export class AppComponent {
   title = 'etiquette_client';
-  public _isTokenValid: boolean;
   private userService: UserService;
-
+  private navigationSubscription: any;
+  
   constructor(userService: UserService, private router: Router) {
     this.userService = userService;
-    this._isTokenValid = false;
   }
 
   ngOnInit() {
-    this.userService.isTokenValid().subscribe({
-        next: (response: any) => {
-          console.log(response.status);
-          if (response.status == 200) {
-            this._isTokenValid = true;
-          }
-      }, error: (response: any) => {
-          console.log(response.error.message);
-          console.log(response.error.status);
-          this._isTokenValid = false;
-          this.router.navigate(['/login']);
-      },})
+    this.navigationSubscription = this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd)
+      )
+      .subscribe(event => {
+        const navEnd = event as NavigationEnd;
+
+        if (navEnd.urlAfterRedirects != "/" && navEnd.urlAfterRedirects != "/login" && navEnd.urlAfterRedirects != "/signin") {
+          this.userService.isTokenValid().subscribe({
+              next: () => {
+                
+            }, error: () => {
+                this.router.navigate(['/login']);
+            },})
+        }
+      });
+  }
+
+  ngOnDestroy() {
+    if (this.navigationSubscription) {
+      this.navigationSubscription.unsubscribe();
+    }
   }
 
 }

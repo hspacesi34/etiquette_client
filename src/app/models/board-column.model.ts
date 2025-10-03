f1182640750c70a621ac2dd5b9e8993e9e467228
hspacesi34 <jarry.cedric34@gmail.com>
2025-10-03 15:22:53 +0200

    feat: implementation creation de boardcolumn

--- src/app/models/board-column.model.ts
+++ src/app/models/board-column.model.ts
@@ -1,6 +1,9 @@
+import { Board } from "./board.model";
+
 export class BoardColumn {
     constructor(
         public id: number = 0,
         public name: string = '',
+        public board: Board = new Board()
     ){}
 }
--- src/app/services/boardcolumn.service.ts
+++ src/app/services/boardcolumn.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { BoardColumn } from '../models/board-column.model';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BoardcolumnService {
+  private apiLink = "/api/boardcolumn";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  addBoardColumn(boardColumn: BoardColumn): Observable<BoardColumn> {
+        const headers = new HttpHeaders({
+            'Content-Type': 'application/json'
+          });
+        return this.http.post<BoardColumn>(this.apiLink+'/add', JSON.stringify(boardColumn), {headers, withCredentials: true});
+  }
 }
